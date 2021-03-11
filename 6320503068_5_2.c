0d6a2b140beafb69b6d38c8988a82c850ddaf32c
Piya <piya.rat@ku.th>
2021-03-11 17:41:49 +0700

    7

--- 6320503068_5_2.c
+++ 6320503068_5_2.c
@@ -1,16 +1,36 @@
 #include<stdio.h>
     void main(){
-        int n,i,j,x,y=0,z;
+        int n,i,x,y,s1,j,s2,check=0;
         scanf("%d",&n);
-        x=n/2;
-        y=n;
-        if(n%2!=0){
-            x++;
+        if(n%2==0){
+            x=n/2;
+            y=2;
+        }
+        else{
+            x=(n/2)+1;
+            y=1;
         }
         if(n>=1&&n<=1000){
             for( i=0 ; i<x ; i++ ){
-                for( j=x-1 ; j>0 ; j-- ){
-                    printf("_");
+                for( j=1 ; j<n ; j++ ){
+                    if(j%x==0){
+                        printf("*");
+                    }
+                    else{
+                        printf("_");
+                    }
+                }
+                printf("\n");
+                x--;
+            }
+            for( i=0 ; i<x+1 ; i++ ){
+                for( j=0 ; j<=n ; j++ ){
+                    if(j==0||j==n-2){
+                        printf("*");
+                    }
+                    else{
+                        printf("_");
+                    }
                 }
                 printf("\n");
             }
--- 6320503068_5_5.c
+++ 6320503068_5_5.c
@@ -1,1 +1,30 @@
 #include<stdio.h>
+int main ()
+{
+    int n,i,j=0,x,y=0;
+    scanf("%d",&n);
+    int ar[3]= {2,3,5},z;
+    for(i=1;j<n; i++)
+    {
+        x=i;
+        while(y!=3)
+        {
+            if(x%ar[y]==0)
+            {
+                x=x/ar[y];
+            }
+            else
+            {
+                y++;
+            }
+        }
+        y=0;
+        if(x==1)
+        {
+            z=i;
+            j++;
+        }
+    }
+    printf("%d",z);
+    return 0;
+}
