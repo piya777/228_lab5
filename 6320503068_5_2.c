99a731c84b57658d191583ec017acaf4bafc096a
Piya <piya.rat@ku.th>
2021-03-11 15:55:56 +0700

    5

--- 6320503068_5_2.c
+++ 6320503068_5_2.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 #include<math.h>
     void main(){
-        int n;
-        scanf("%d",&n)
+        int n,i,j;
+        scanf("%d",&n);
+        x = ceil(n/2);
+        y = floor(n/2);
         if(n>=1&&n<=1000){
-            x = ceil(n/2);
-
+            for( i=0 ; i<x ; i++ ){
+                for( j=0 ; j<n ; j++ ){
+                    if
+                }
+            }
         }
     }
