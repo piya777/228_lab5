eb73903cc7e6c2533a601780fee43b31640a5787
Piya <piya.rat@ku.th>
2021-03-11 15:22:06 +0700

    2

--- 6320503068_5_5.c
+++ 6320503068_5_5.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 
     void main(){
-        int n,check=1,i=2;
+        int n,check=2,i=2;
         scanf("%d",&n);
         if(n==1){
             printf("1");
         }
-        else{
+        else if(n>1){
             while(check<=n){
-                if(i%2==0||i%3==0||i%5==0){
+                if(i%7!=0 && i%11!=0 && i%13!=0 && i%2==0||i%3==0||i%5==0){
                     if(check==n){
-                        check++;
                         break;
                     }
                     check++;
