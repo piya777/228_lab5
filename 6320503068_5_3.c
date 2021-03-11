7c92e4e4df4a8715ab17a3a1f0d8d6b26c212f9b
Piya <piya.rat@ku.th>
2021-03-11 15:33:12 +0700

    4

--- 6320503068_5_3.c
+++ 6320503068_5_3.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 
     void main(){
+        int n,check=2,i=2;
+        scanf("%d",&n);
+        if(n==1){
+            printf("1");
+        }
+        else if(n>1){
+            while(check<=n){
+                if(i%7!=0 && i%11!=0 && i%13!=0 && i%2==0||i%3==0||i%5==0){
+                    if(check==n){
+                        break;
+                    }
+                    check++;
+                }
+                i++;
+            }
+            printf("%d",i);
+        }
 
     }
